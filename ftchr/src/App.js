c15b10acdbfbf8c31b27cf93674c6ffc9ad1913e
Becca <95663074+Acanthodoris@users.noreply.github.com>
2022-03-07 14:28:31 -0800

    added navbar and footer

--- ftchr/src/App.js
+++ ftchr/src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Footer from "./components/Footer.js";
+import Header from "./components/Header.js";
+import {Routes, Route, BrowserRouter} from "react-router-dom";
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className=""></h1>
+      <BrowserRouter>
+        <Header />
+          <Routes></Routes>
+        <Footer />
+      </BrowserRouter>
     </div>
   );
 }
