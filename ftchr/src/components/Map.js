1fff2863f667a9c1eb10866af4ce13bed32dd61a
Andrew Tran <andrewtranmsw@gmail.com>
2022-03-09 22:54:08 -0800

    map and postcards in flex div, plots work on map, will need to work on circle and custom icons next

--- ftchr/src/components/Map.js
+++ ftchr/src/components/Map.js
@@ -1,7 +1,16 @@
 import React from "react";
-import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import {
+  MapContainer,
+  TileLayer,
+  Marker,
+  Popup,
+  Circle,
+  CircleMarker,
+} from "react-leaflet";
 import { Icon } from "leaflet";
 import "../styles/style.css";
+
+const redOptions = { color: "red" };
 
 export default function Map() {
   return (
@@ -13,7 +22,17 @@ export default function Map() {
         />
         <Marker position={[47.606, -122.332]}>
           <Popup>
-            A pretty CSS3 popup. <br /> Easily customizable.
+            Hangout at Discovery Park <br /> 2PM
+          </Popup>
+        </Marker>
+        <Marker position={[47.606, -122.329]}>
+          <Popup>
+            Chilling with my doggo, come hang! <br /> 5PM
+          </Popup>
+        </Marker>
+        <Marker position={[47.606, -122.331]}>
+          <Popup>
+            Pet my cat! <br /> 3PM
           </Popup>
         </Marker>
       </MapContainer>
--- ftchr/src/pages/Dashboard.js
+++ ftchr/src/pages/Dashboard.js
@@ -1,11 +1,12 @@
 import React from "react";
 import Postcard from "../components/Postcard";
 import Map from "../components/Map";
+import "../styles/style.css";
 
 export default function Dashboard() {
   return (
-    <div>
-      <div>
+    <div className="dashboard-flex">
+      <div className="postcards">
         <Postcard />
         <Postcard />
         <Postcard />
