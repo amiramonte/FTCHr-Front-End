9cb04c34d02f7fe72f47fec4a175751cc8902f75
Andrew Tran <andrewtranmsw@gmail.com>
2022-03-15 09:57:47 -0700

    added a few lines to widget and fixed a typo

--- src/components/NewPostForm.js
+++ src/components/NewPostForm.js
@@ -10,7 +10,7 @@ export default function FormPropsTextFields({
   posts,
   getAllPost,
   photo,
-  setLocation
+  setLocation,
 }) {
   const [token, setToken] = useState("");
   const [formState, setFormState] = useState({
@@ -47,27 +47,26 @@ export default function FormPropsTextFields({
         getAllPost();
       });
 
-      var options = {
-        enableHighAccuracy: true,
-        timeout: 5000,
-        maximumAge: 0
-      };
-      
-      function success(pos) {
-        var crd = pos.coords;
-        setLocation(crd);
-        console.log('Your current position is:');
-        console.log(`Latitude : ${crd.latitude}`);
-        console.log(`Longitude: ${crd.longitude}`);
-        console.log(`More or less ${crd.accuracy} meters.`);
-        set
-      }
-      
-      function error(err) {
-        console.warn(`ERROR(${err.code}): ${err.message}`);
-      }
-      
-      navigator.geolocation.getCurrentPosition(success, error, options);
+    var options = {
+      enableHighAccuracy: true,
+      timeout: 5000,
+      maximumAge: 0,
+    };
+
+    function success(pos) {
+      var crd = pos.coords;
+      setLocation(crd);
+      console.log("Your current position is:");
+      console.log(`Latitude : ${crd.latitude}`);
+      console.log(`Longitude: ${crd.longitude}`);
+      console.log(`More or less ${crd.accuracy} meters.`);
+    }
+
+    function error(err) {
+      console.warn(`ERROR(${err.code}): ${err.message}`);
+    }
+
+    navigator.geolocation.getCurrentPosition(success, error, options);
   };
 
   return (
