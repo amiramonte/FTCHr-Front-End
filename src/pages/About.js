6d8efcc2193e34a85fd11fb2e95359a988008a64
Becca <95663074+Acanthodoris@users.noreply.github.com>
2022-03-15 19:49:06 -0700

    updating to pull

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,15 +1,18 @@
 import React from "react";
-import Logo from '../assets/logowithphrase.png'
+import Logo from '../assets/logowithphrasetrimmed.png'
 
 export default function About() {
     return (
       <div className='homeStyle' >
           <img src={Logo}
           alt="A dog and a cat in a circle"/>
-          <p>
-          <a href="/signup">Sign up</a>   
-          <a href="/login"> or Login</a>
-          to join the fun!</p>
+          <p>We wanted to help create and harbor a community for people to connect with other people lovers localy to help socialize. Weather it be socializing with other people, pets or pet rocks, everyone deservse a friend.</p>
+
+          <p>Lovingly created by:</p>
+          <a href="https://github.com/AndrewTranMSW">Andrew Tran</a> 
+          <a href="https://github.com/amiramonte">Alex Miramontes</a>
+          <a href="https://github.com/truont2">Takara Truong</a>
+          <a href="https://github.com/Acanthodoris">Rebeccah Mullan</a>
       </div>
     )
   }
