e280ee36685d24f0e3b5a0781adca46600395e00
Takara Truong <truont2@gmail.com>
2022-03-16 15:29:10 -0700

    changed messenger page socket connection

---
+++ socket/utils/helper.js
@@ -0,0 +1,7 @@
+let deployed = true;
+
+let prefixURL = deployed
+  ? "https://ftchrbackend.herokuapp.com"
+  : "http://localhost:3001";
+
+export default prefixURL;
--- src/pages/Messenger.js
+++ src/pages/Messenger.js
@@ -29,6 +29,7 @@ export default function Messenger({ user }) {
 
   // connect to the socket server
   useEffect(() => {
+    // put deployed socket io heroku page here
     socket.current = io("ws://localhost:8900");
     socket.current.on(
       "getMessage",
